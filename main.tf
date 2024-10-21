b25a49a51327f046d9fb48346bfc2d36f962b385
Deepika035 <deepu035200@gmail.com>
2024-10-21 21:47:56 +0530

    added main

--- main.tf
+++ main.tf
@@ -2,22 +2,22 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     okta = {
-      source = "okta/okta"
+      source  = "okta/okta"
       version = "4.9.1"
     }
   }
 }
-  
+
 # Configure the Okta provider
 provider "okta" {
   org_name  = "doosan"
   base_url  = "oktapreview.com"
   api_token = "00nkE3xHVLURZJC-FVYbb2LYTyLM4HEXZWJ0AODQ3t"
 }
- 
+
 resource "okta_user" "test" {
-  first_name           = "Terraform"
-  last_name            = "Test"
-  login                = "terraform.test@gmail.com"
-  email                = "terraform.test@gmail.com"
+  first_name = "Terraform"
+  last_name  = "Test"
+  login      = "terraform.test@gmail.com"
+  email      = "terraform.test@gmail.com"
 }
